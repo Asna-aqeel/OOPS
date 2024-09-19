d5d16aff145c7500fb3a11d13cd4276a186d10ef
Asna-aqeel <sp24-bse-016@cuilahore.edu.pk>
2024-09-19 15:44:27 +0500

    Add files via upload

---
+++ Artist.java
@@ -0,0 +1,18 @@
+public class Artist{
+
+	private String name;
+
+
+	Artist(String name){
+	this.name = name;
+	}
+	
+	public String getname(){
+		return name;
+	}
+	
+	public void display(){
+		System.out.println("Artist name is " + name);	
+	}
+
+}
